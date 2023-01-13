aa40d64308945029ca8a535a05d620ac06b3c3fc
Lucas Marques Garrido <lucas-mgarrido20@outlook.com>
2023-01-12 23:35:18 -0300

    Adicionando anotação ao banco de dados

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const db = require('./loaders/mongoose.js')
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+const Notas = require('./models/noteModel')
 
 //Conectar banco de dados
 db()
@@ -10,6 +11,19 @@ db()
 app.use(express.json());
 app.use(routes);
 
+
+//Adicionando anotação ao banco de dados
+app.use(express.urlencoded({extended: true}))
+
+app.post('/dev', async(req, res) => {
+    //const titulo = req.body.tituloadd
+    //const anotacao = req.body
+    //console.log(anotacao)
+
+    const anotacao = new Notas(req.body)
+    await anotacao.save()
+    res.redirect('/')
+})
 
 //app.use('view engine' , 'ejs')
 
