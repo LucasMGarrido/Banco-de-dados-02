0121267b12fecd458cdbcfd69dc676ab8b04310c
Lucas Marques Garrido <lucas-mgarrido20@outlook.com>
2023-01-19 22:09:51 -0300

    Corringo desformatação do front

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -2,92 +2,11 @@ const noteModel = require('../models/noteModel');
 
 
 class noteController{
-    async store(req, res){ //cria o nota
-        try{
-            const newNote = req.body;
-
-            if(!newNote){
-                return res.status(404).json({ mensagem: "Notas vazias não são aceitas" })
-            }
-            const createdNote = await noteModel.create(newNote);
-
-            return res.status(200).json(createdNote);
-        }catch(error){
-            return res.status(404).json({ mensagem: "Erro na criação" });
-        }
-    }
-
-    async index(req, res){ //listar os produtos
-        const notes = await noteModel.find();
-
-        return res.status(200).json({ notes });
-    }
-
-    async show(req, res){ //listar um produto específico
-        try{
-
-            const result = await noteModel.aggregate([
-                {
-                  '$search': {
-                    'index': 'search-text',
-                    'text': {
-                      'query': req.query.title,
-                      'path': {
-                        'wildcard': '*'
-                      }
-                    }
-                  }
-                }
-            ]);
-
-            if(!result){
-              return res.status(404).json({ mensagem: "A nota não existe" });
-            }else{
-              return res.status(200).send(result);
-            }
-        }catch(error){
-            return res.status(404).json({ mensagem: "título inválido" })
-        }
-    }
-
-    async updateeee(req, res){ //atualiza o produto
-        try{
-            const { id } = req.params;
-
-            await noteModel.replaceOne({title:req.params.title}, req.body);
-
-            return res.status(200).json({ mensagem: "Nota atualizada" })
-        }catch(error){
-            return res.status(404).json({ mensagem: "Erro de atualização" })
-        }
-    }
-
-    async destroy(req, res){ //deleta o produto
-        try{
-
-            const note = req.body;
-
-           const noteDeleted = await noteModel.deleteOne(note);
-
-            if(!noteDeleted){
-                return res.status(404).json({ mensagem: "A nota não existe" });
-            }else{
-                return res.status(200).json({ mensagem: "Nota deletada" });
-            }
-        }catch(error){
-            return res.status(404).json({ mensagem: "Não foi possível deletar" });
-        }
-
-    }
-
     async ler (req, res){
         const items = await noteModel.find()
 
         res.render('../views/front.ejs', {items : items})
     }
-
-
-
 
     async deletar (req, res) {
         const id = req.params.id
@@ -97,16 +16,16 @@ class noteController{
 
     async pesquisar (req, res) {
         const pesquisa = req.params.texto
-        console.log(pesquisa)
+
         const items = await noteModel.find({$text:{$search:pesquisa}}, {score:{$meta:'textScore'}})
-        console.log(items)
+
         res.render('../views/front.ejs', {items : items})
     }
 
     async renderizarPagina (req, res){
-        console.log("tá entrando no renderizar")
+
         const id = req.params.id
-        console.log(id)
+
         const items = await noteModel.findById(id)
         res.render('../views/edit.ejs', {items})
     }
@@ -118,7 +37,6 @@ class noteController{
             content: req.body.content
         }
         await noteModel.findByIdAndUpdate(id, nota)
-        console.log(id, nota)
 
         res.redirect('/dev')
     }
--- routes.js
+++ routes.js
@@ -13,17 +13,4 @@ routes.get('/buscar/:texto', noteController.pesquisar);
 routes.put('/atualizar/:id', noteController.update);
 routes.get('/edit/atualizar/:id', noteController.renderizarPagina);
 
-
-
-routes.post('/notes', noteController.store);
-routes.get('/notes', noteController.index);
-routes.get('/notes/:id', noteController.show);
-//routes.put('/notes/:id', noteController.update);
-//routes.delete('/notes', noteController.destroy);
-
 module.exports = routes;
-
-/*
-<ion-icon name="search-outline"></ion-icon>
-                        <input type="text" name="buscar" placeholder="Buscar" />
-*/
